39d794f0ac6d2925784a60891a1eca9ebdae2f5a
Md Imam Ahasan <emamahasane@gmail.com>
2021-11-10 00:53:46 +0600

    Add files via upload

---
+++ python_practice/Try_And_Except.py
@@ -0,0 +1,23 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 10:11:57 2019
+
+@author: Imam Ahasan
+"""
+name = 6
+try:
+    if name > 3:
+        print("True")
+        
+except:
+    print("Something is wrong, Please try again.")
+    
+
+try:
+    c = 90
+    if c >= 90:
+        print("True")
+                
+except:
+    print("Something is wromg.Try again.")
---
+++ python_practice/while_continue.py
@@ -0,0 +1,15 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 07:31:51 2019
+
+@author: Imam Ahasan
+"""
+
+c = 0
+while c < 5:
+    if c == 3:
+        continue
+    print(c)
+    c += 1
+    
