0ae0de32250e589387a58c693fa4852a888a9314
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-20 05:09:52 +0600

    Add files via upload

---
+++ Coding_Skills/16_modules.py
@@ -0,0 +1,22 @@
+import math
+
+a = math.pi
+print(a)
+
+b = math.log10(1000)
+print(b)
+
+
+import calendar
+
+# c = calendar(2018, 6)
+c = calendar.month(2018, 6)
+print(c)
+
+d = calendar.isleap(2018)
+print(d)
+
+e = calendar.isleap(2016)
+print(e)
+
+print(dir(calendar))
