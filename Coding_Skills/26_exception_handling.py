c00e61b6538963d85f43368a4673f7197c500aea
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-20 19:53:24 +0600

    Add files via upload

--- Coding_Skills/26_exception_handling.py
+++ Coding_Skills/26_exception_handling.py
@@ -1,2 +1,16 @@
 
 # Exceptions are errors detected at the time of program execution
+def bacis_calculator(n1, n2):
+    try:
+        result = n1 / n2
+    
+    except Exception as e:
+        print(f"exception occured: {e}")
+        result = None
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+z = bacis_calculator(a, b)
+print(z)
+
--- Coding_Skills/27_exception_handling.py
+++ Coding_Skills/27_exception_handling.py
@@ -1,1 +1,19 @@
 # Exceptions are errors detected at the time of program execution
+
+def bacis_calculator(n1, n2):
+    try:
+        result = int(n1) / int(n2)
+
+    except ZeroDivisionError as e:
+        print("Division by zero exception")
+        result = None
+    
+    except Exception as e:
+        print(f"exception occured: {type(e).__name__}")
+        result = None
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+z = bacis_calculator(a, b)
+print(z)
