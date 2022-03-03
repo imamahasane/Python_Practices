e37ea56b307c4b9efb687acb2e91bae29c800fad
Md Imam Ahasan <emamahasane@gmail.com>
2022-03-04 04:25:04 +0600

    Add files via upload

---
+++ EdX Computer Science and Programming/swapeing.py
@@ -0,0 +1,10 @@
+
+a = 5
+b = 10
+print(f"Befor Swaping A = {a}, and B = {b}")
+
+a = a + b
+b = a - b
+a = a - b
+
+print(f"After Swaping A = {a}, and B = {b}")
---
+++ EdX Computer Science and Programming/test.py
@@ -0,0 +1,3 @@
+
+
+print("Imam Ahasan")
