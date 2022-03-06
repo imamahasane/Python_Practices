d30fc1c53ce732b0e9de29d97c70094c219ae94b
Md Imam Ahasan <emamahasane@gmail.com>
2022-03-07 03:58:59 +0600

    Add files via upload

---
+++ EdX Computer Science and Programming/breck.py
@@ -0,0 +1,9 @@
+
+mysum = 0
+for i in range(5, 11, 2):
+    mysum += i
+    
+    if mysum == 5:
+        break
+    
+print(mysum)
---
+++ EdX Computer Science and Programming/soaping with variable.py
@@ -0,0 +1,14 @@
+
+x = 1
+y = 2
+temp = y
+print(f"Befor Swaping X = {x}, and Y = {y}")
+
+
+y = x
+# print(y)
+
+x = temp
+# print(x)
+
+print(f"After Swaping X = {x}, and Y = {y}")
